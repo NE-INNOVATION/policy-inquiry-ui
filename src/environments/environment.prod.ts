d7945f1d29ecc6fddd8967eb76714ab74bdaa705
afu174 <35349205+afu174@users.noreply.github.com>
2020-09-10 12:20:16 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  gatewayUrl : 'http://istio-ingressgateway-istio-system.apps.azureopenshift.neinnovation-ocp.com/api/policy/policyinquiry/getdetails/'
+  gatewayUrl : 'http://policy-inquiry-api-development-ui.apps.azureocp.neinnovation-ocp.com/api/policy/PolicyInquiry/getdetails/'
 };
 
