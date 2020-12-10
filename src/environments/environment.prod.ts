35a962a0abf7ff374603d301342902a9f4f5c1f4
ganeshbhogle88 <34191283+ganeshbhogle88@users.noreply.github.com>
2020-12-10 14:11:19 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  gatewayUrl : 'http://policy-inquiry-api-development-ui.apps.azureocp.neinnovation-ocp.com/api/policy/PolicyInquiry/getdetails/'
+  gatewayUrl : 'http://policy-inquiry-api-development.apps.azureneocp.neinnovation-ocp.com/api/policy/PolicyInquiry/getdetails/'
 };
 
